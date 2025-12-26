6b7e0f5f77565ba86291402cf8ed5c29d507290d
erickknsilva <erick.nsilva100@gmail.com>
2025-12-26 14:34:49 -0300

    Corrigindo as configurações.

--- src/main/java/com/buildrun/encurtadorlinkfbr/core/usecase/CreateUserCase.java
+++ src/main/java/com/buildrun/encurtadorlinkfbr/core/usecase/CreateUserCase.java
@@ -1,32 +1,36 @@
-package com.buildrun.encurtadorlinkfbr.core.usecase;
+    package com.buildrun.encurtadorlinkfbr.core.usecase;
 
 
-import com.buildrun.encurtadorlinkfbr.core.domain.User;
-import com.buildrun.encurtadorlinkfbr.core.port.in.CreateUserPortIn;
-import com.buildrun.encurtadorlinkfbr.core.port.out.UserRepositoryPortOut;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Service;
+    import com.buildrun.encurtadorlinkfbr.core.domain.User;
+    import com.buildrun.encurtadorlinkfbr.core.port.in.CreateUserPortIn;
+    import com.buildrun.encurtadorlinkfbr.core.port.out.UserRepositoryPortOut;
+    import org.slf4j.Logger;
+    import org.slf4j.LoggerFactory;
+    import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+    import org.springframework.stereotype.Service;
 
-@Service
-public class CreateUserCase implements CreateUserPortIn {
+    @Service
+    public class CreateUserCase implements CreateUserPortIn {
 
-    private static final Logger logger = LoggerFactory.getLogger(CreateUserCase.class);
-    private final UserRepositoryPortOut userRepositoryPortOut;
+        private static final Logger logger = LoggerFactory.getLogger(CreateUserCase.class);
+        private final UserRepositoryPortOut userRepositoryPortOut;
+        private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public CreateUserCase(UserRepositoryPortOut userRepositoryPortOut) {
-        this.userRepositoryPortOut = userRepositoryPortOut;
+        public CreateUserCase(UserRepositoryPortOut userRepositoryPortOut) {
+            this.userRepositoryPortOut = userRepositoryPortOut;
+            this.bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        }
+
+        @Override
+        public User execute(User userRequest) {
+
+            logger.info("Creating user {}", userRequest.getEmail());
+            userRequest.setPassword(bCryptPasswordEncoder.encode(userRequest.getPassword()));
+            User userCreated = userRepositoryPortOut.save(userRequest);
+            logger.info("User created {}", userCreated.getUserId());
+
+            return userCreated;
+        }
+
+
     }
-
-    @Override
-    public User execute(User userRequest) {
-
-        logger.info("Creating user {}", userRequest.getEmail());
-        User userCreated = userRepositoryPortOut.save(userRequest);
-        logger.info("User created {}", userCreated.getUserId());
-
-        return userCreated;
-    }
-
-
-}
